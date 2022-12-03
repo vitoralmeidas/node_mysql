6e3934ea73879cfc0432df70cbffbd0672fb3bf3
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-12-03 20:42:14 -0300

    CRUD endereco _usuario --> ok

--- app.js
+++ app.js
@@ -73,9 +73,77 @@ app.delete("/usuarios/:id_usuario", (req, res) => {
 
 ////////////////////////// ENDEREÇOS ///////////////////////////////
 
-app.get("/enderecos-usuario/:id_usuario", (req, res) => {});
+//get by user id
+app.get("/enderecos-usuario/:id_usuario", (req, res) => {
+  con.query(
+    "SELECT * FROM endereco_usuario WHERE id_usuario = ?",
+    req.params.id_usuario,
+    function (err, results, fields) {
+      if (err) res.json({ message: err });
+      else res.json({ results });
+    }
+  );
+});
 
-//get
+// get by address id
+app.get("/enderecos-usuario/:id_endereco_usuario", (req, res) => {
+  con.query(
+    "SELECT * FROM endereco_usuario WHERE id_endereco_usuario = ?",
+    req.params.id_endereco_usuario,
+    function (err, results) {
+      if (err) res.json({ err });
+      else res.json({ results });
+    }
+  );
+});
+
+// post new address
+app.post("/enderecos-usuario/", (req, res) => {
+  const data = req.body;
+  con.query(
+    "INSERT INTO endereco_usuario SET ?",
+    data,
+    function (err, results) {
+      if (err) res.json({ err });
+      else res.json({ results });
+    }
+  );
+});
+
+// put
+app.put("/enderecos-usuario/:id_endereco_usuario", (req, res) => {
+  const data = [
+    req.body.id_usuario,
+    req.body.logradouro,
+    req.body.numero,
+    req.body.cidade,
+    req.body.uf,
+    req.body.cep,
+    req.body.bairro,
+    req.body.complemento,
+    req.params.id_endereco_usuario,
+  ];
+  con.query(
+    "UPDATE endereco_usuario SET id_usuario = ?, logradouro = ?, numero = ?, cidade = ?, uf = ?, cep = ?, bairro = ?, complemento = ? WHERE id_endereco_usuario = ?",
+    data,
+    function (err, results, fields) {
+      if (err) res.json({ err });
+      else res.json({ results });
+    }
+  );
+});
+
+// delete
+app.delete("/enderecos-usuario/:id_endereco_usuario", (req, res) => {
+  con.query(
+    "DELETE FROM endereco_usuario WHERE id_endereco_usuario = " +
+      req.params.id_endereco_usuario,
+    function (err, results) {
+      if (err) res.json({ err });
+      else res.json({ results });
+    }
+  );
+});
 
 // listen for requests
 const PORT = 3000;
